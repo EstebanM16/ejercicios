fdea30612a18a05bb0e66112651095fc068281a7
EstebanM16 <estebanmontoyaarismendy@gmail.com>
2023-02-22 08:02:53 -0500

    almacen_proyect

---
+++ almacen_wc.py
@@ -0,0 +1,47 @@
+
+articulo1 = "Zapatos"
+articulo2 = "tenis"
+articulo3 = "camisetas"
+articulo4 = "jeans"
+
+precio1 = 350000
+precio2 = 280000
+precio3 = 175000
+precio4 = 200000
+
+
+
+
+
+print (articulo1,"  $",precio1);
+print (articulo2,"  $",precio2);
+print (articulo3,"  $",precio3);
+print (articulo4,"  $",precio4);
+
+total= (precio1 + precio2 + precio3 + precio4)
+
+print ("Total : $" ,total);
+
+
+promedio= (total/4)
+print ("Promedio :$", promedio)
+
+
+
+sub_jean= (precio4* 0.06)
+
+
+new_jean= (precio4 + sub_jean);
+
+
+dism= precio1 - 0.08
+
+print  ("nuevos precios")
+print (articulo4 ,"  : $" ,new_jean)
+
+print (articulo1 ," : $",dism)
+
+
+
+
+
